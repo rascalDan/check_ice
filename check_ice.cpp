40cc04ae1625d4c4d32e040c59431e405013b2a0
Dan Goodliffe <dan@randomdan.homeip.net>
2022-05-16 16:09:46 +0100

    Reformat with new clang-format

--- check_ice.cpp
+++ check_ice.cpp
@@ -8,13 +8,16 @@ AdHocFormatter(EndpointFmt, "%?:%? -h %? -p %?");
 AdHocFormatter(Good, " OK: %? %?ms");
 AdHocFormatter(Bad, " Bad: %? %?");
 
-template<typename T> T & atLeast(T & v, const T & v2)
+template<typename T>
+T &
+atLeast(T & v, const T & v2)
 {
 	return (v = std::max(v, v2));
 }
 
 template<typename F>
-auto stopwatch(const F & f)
+auto
+stopwatch(const F & f)
 {
 	auto t_start = std::chrono::steady_clock::now();
 	f();
@@ -30,6 +33,7 @@ main(int argc, char ** argv)
 	uint32_t port;
 
 	po::options_description opts("Check ICE object");
+	// clang-format off
 	opts.add_options()
 		("host,H", po::value(&host)->default_value("localhost"), "Host")
 		("port,p", po::value(&port)->required(), "Port")
@@ -39,14 +43,14 @@ main(int argc, char ** argv)
 		("crit,c", po::value(&crit)->default_value(5000), "Critical at millisecond")
 		("help,h", "Show help")
 		;
+	// clang-format on
 	try {
 		po::variables_map vm;
 		po::store(po::command_line_parser(argc, argv)
-				.positional(po::positional_options_description()
-					.add("host", 1)
-					.add("object", -1))
-				.options(opts)
-				.run(), vm);
+						  .positional(po::positional_options_description().add("host", 1).add("object", -1))
+						  .options(opts)
+						  .run(),
+				vm);
 
 		if (vm.count("help")) {
 			std::cerr << opts << std::endl;
@@ -61,12 +65,14 @@ main(int argc, char ** argv)
 		for (const auto & object : objects) {
 			try {
 				auto p = ic->stringToProxy(EndpointFmt::get(object, protocol, host, port));
-				auto t_taken_ms = stopwatch([&p]{
+				auto t_taken_ms = stopwatch([&p] {
 					p->ice_ping();
 				});
 				Good::write(std::cout, object, t_taken_ms);
-				if (t_taken_ms > crit) atLeast(rtn, 2);
-				if (t_taken_ms > warn) atLeast(rtn, 1);
+				if (t_taken_ms > crit)
+					atLeast(rtn, 2);
+				if (t_taken_ms > warn)
+					atLeast(rtn, 1);
 			}
 			catch (const std::exception & ex) {
 				Bad::write(std::cout, object, ex.what());
@@ -82,4 +88,3 @@ main(int argc, char ** argv)
 		return 3;
 	}
 }
-
